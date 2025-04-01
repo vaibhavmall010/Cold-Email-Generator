f6c94033608dd1cd662103413ed341f4ca8cfc74
Vaibhav Mall <mallvaibhav010@gmail.com>
2025-04-02 00:08:26 +0200

    Update from langchain_groq import ChatGroq.py

--- from langchain_groq import ChatGroq.py
+++ from langchain_groq import ChatGroq.py
@@ -2,7 +2,7 @@ from langchain_groq import ChatGroq
 
 llm = ChatGroq(
         temperature =0,
-        groq_api_key = "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",
+        groq_api_key = "gsk_4koZtUxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
         model_name= "llama-3.3-70b-versatile")
 
 response = llm.invoke("Capital of india")
